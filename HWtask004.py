297dfb2930ec91aa2354fa62eae4850e085be75e
Kirk Dirk <jackalex@ya.ru>
2022-09-26 23:21:47 +0500

    add code

--- HWtask004.py
+++ HWtask004.py
@@ -3,4 +3,8 @@
 # точек в этой четверти (x и y).
 
 quarternumber = int(input('Введите номер четверти плоскости: '))
-if 4 < quarternumber < 1: print('Введено неверное число')
+if 4 < quarternumber or quarternumber < 1: print('Введено неверное число')
+if quarternumber == 1: print('Диапазон возможных координат: x > 0, y > 0')
+if quarternumber == 2: print('Диапазон возможных координат: x < 0, y > 0')
+if quarternumber == 3: print('Диапазон возможных координат: x < 0, y < 0')
+if quarternumber == 4: print('Диапазон возможных координат: x > 0, y < 0')
